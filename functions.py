9b85347a753df03db0476e8a6bd6d4080c09e38e
cyberiolabs <cyberiolabs@gmail.com>
2022-08-14 12:56:12 +0300

    code added

--- functions.py
+++ functions.py
@@ -7,12 +7,14 @@ ops()
 def profile():
     first_name = "Ramadhan" #strings
     second_name = "Ibrahim" #strings
+    age = 26
     pro = "Cyber Security Engineer | Python Developer," # strings
 
     print("")
     print("==========================================")
     print("My name is ", first_name,second_name)
     print("I am ", pro)
+    print("Am",age, "years" )
     print("==========================================")
 
 profile()
