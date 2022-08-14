24aa59e19a449677b85403d83076a31a5b4eeb95
cyberiolabs <cyberiolabs@gmail.com>
2022-08-14 12:53:12 +0300

    var & fuct adedd

---
+++ functions.py
@@ -0,0 +1,18 @@
+import os
+def ops():
+    cmd = 'cls' # for windows
+    os.system(cmd)
+ops()
+
+def profile():
+    first_name = "Ramadhan" #strings
+    second_name = "Ibrahim" #strings
+    pro = "Cyber Security Engineer | Python Developer," # strings
+
+    print("")
+    print("==========================================")
+    print("My name is ", first_name,second_name)
+    print("I am ", pro)
+    print("==========================================")
+
+profile()
