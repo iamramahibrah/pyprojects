faf89c65d1c2c3e66908b207ea22b7f492c2625f
cyberiolabs <cyberiolabs@gmail.com>
2022-08-14 12:44:04 +0300

    variables project commited

---
+++ variables.py
@@ -0,0 +1,16 @@
+import os
+
+
+first_name = "Ramadhan" #string
+second_name = "Ibrahim" #string
+
+cmd = 'cls' # for windows
+os.system(cmd)
+print("")
+print("==========================================")
+pro = "Cyber Security Engineer | Python Developer,"
+print("My name is ", first_name,second_name)
+print("I am ", pro)
+print("==========================================")
+
+
